de11478152e0f383ae6d1ac0d08f6c4ab8a71121
Mahmoud Magdy <m.magdy5858@gmail.com>
2025-06-19 11:03:13 +0300

    add layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
-import { Stack } from "expo-router";
+import { persistor, store } from "@/store";
+import { Slot } from "expo-router";
+import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function RootLayout() {
-  return <Stack />;
+
+  StatusBar.setBarStyle("dark-content");
+  return(
+    <Provider store={store}>
+      <PersistGate  persistor={persistor}>
+        <SafeAreaProvider>
+          <Slot />
+        </SafeAreaProvider>
+      </PersistGate>
+    </Provider>
+  );
 }
